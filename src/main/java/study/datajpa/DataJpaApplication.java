a7fcfb87dd960fa6fc16ba427552793d41ce0cdb
yuntaeLee <leeyt1201@gmail.com>
2022-06-14 20:31:39 +0900

    Add base entity

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -3,15 +3,26 @@ package study.datajpa;
 import com.p6spy.engine.spy.P6SpyOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import study.datajpa.p6spy.P6SpyPrettySqlFormatter;
 
 import javax.annotation.PostConstruct;
+import java.util.Optional;
+import java.util.UUID;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataJpaApplication.class, args);
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.of(UUID.randomUUID().toString());
     }
 
     @PostConstruct
--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -5,10 +5,10 @@ import lombok.*;
 import javax.persistence.*;
 
 @Entity
-@Getter
+@Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
-public class Member {
+public class Member extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "member_id")
